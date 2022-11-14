dfe4f75df584e2d1e32189ce8da22afc76eb2ce1
Alan Poon <brightshadowx@gmail.com>
2022-11-14 09:54:05 +0800

    t

--- install_wasmcloud.sh
+++ install_wasmcloud.sh
@@ -1,3 +1,4 @@
-if [[ ! -f "./bin/wasmcloud_host" ]]; then
-  wget -c https://github.com/wasmCloud/wasmcloud-otp/releases/download/v0.50.2/x86_64-linux.tar.gz -O - | tar -xz
+if [[ ! -f "./bin/wasmcloud_host" ]]; 
+  then
+    wget -c https://github.com/wasmCloud/wasmcloud-otp/releases/download/v0.50.2/x86_64-linux.tar.gz -O - | tar -xz
 fi
